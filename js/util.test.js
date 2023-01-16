8b092ab6d2ff3dc2f931e0d8ec478819c45366f1
Konrad <konslo96@gmail.com>
2023-01-16 21:10:28 +0100

    Write integration tests

--- js/util.test.js
+++ js/util.test.js
@@ -1,6 +1,16 @@
-const { generateText } = require("./util");
+const { generateText, checkAndGenerate, validateInput } = require("./util");
 
 test("Should output name and age", () => {
   const text = generateText("Konrad", 26);
   expect(text).toBe("Konrad (26 years old)");
+});
+
+test("Should generate a valid text output", () => {
+  const text = checkAndGenerate("Konrad", 26);
+  expect(text).toBe("Konrad (26 years old)");
+});
+
+test("Should return validation boolean", () => {
+  const value = validateInput("Konrad", true, false);
+  expect(value).toBe(true);
 });
