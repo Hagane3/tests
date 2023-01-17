2d406779f139ea50d9987dc1b420cf407c25e758
Konrad <konslo96@gmail.com>
2023-01-17 14:12:41 +0100

    Add e2e test

--- js/util.test.js
+++ js/util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate, validateInput } = require("./util");
 
 test("Should output name and age", () => {
@@ -14,3 +15,19 @@ test("Should return validation boolean", () => {
   const value = validateInput("Konrad", true, false);
   expect(value).toBe(true);
 });
+
+test("Should create an element with text and correct class", async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["---window-size=1920, 1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto("file:///A:/LEARNING/tests/js/index.html");
+  await page.click("input#name");
+  await page.type("input#name", "Konrad");
+  await page.click("input#age");
+  await page.type("input#age", "26");
+  await page.click("#btnAddUser");
+  const finalText = await page.$eval(".user-item", (el) => el.textContent);
+  expect(finalText).toBe("Konrad (26 years old)");
+}, 10000);
