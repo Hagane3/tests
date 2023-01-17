206a2cff6179f66a83e2743a4cd42410da86cb8f
Konrad <konslo96@gmail.com>
2023-01-17 14:47:33 +0100

    add async code test

---
+++ js-async/util.test.js
@@ -0,0 +1,7 @@
+const { loadTitle } = require("./util");
+
+test("Should print an uppercase text", () => {
+  loadTitle().then((title) => {
+    expect(title).toBe("DELECTUS AUT AUTEM");
+  });
+});
